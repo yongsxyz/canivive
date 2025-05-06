1ed9cf27b3b22c57628835acb61f325776b0f05d
yongsxyz <iyong.wanien35@gmail.com>
2025-05-07 04:34:37 +0700

    Update

--- gameengine/src/ecs/component/physics/VehicleRayCastComponent.ts
+++
@@ -1,8 +0,0 @@
-import Rapier from '../../../physics/rapier.js'
-import { Component } from '../../../../../shared/component/Component.js'
-
-export class VehicleRayCastComponent extends Component {
-  constructor(entityId: number, public raycastController: Rapier.DynamicRayCastVehicleController) {
-    super(entityId)
-  }
-}
